c70210bb6edac0f5e311bff81da93103a1017f03
Stoian <stoian@debian.debian.bg>
2022-08-01 00:32:17 +0300

    separate all DIs in project

--- MyWebApplication.Api/Program.cs
+++ MyWebApplication.Api/Program.cs
@@ -8,10 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
     // Add services to the container.
     builder.Services.AddMyWebApplication();
     builder.Services.AddWebInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddMyWebApi();
 };
 
 
